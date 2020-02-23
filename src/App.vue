714df7f75645ec5e138a731f220a24482862307b
zenkers <dpswanson2010@gmail.com>
2020-02-23 11:25:29 -0500

    cleanup for demo

--- src/App.vue
+++ src/App.vue
@@ -4,11 +4,15 @@
       <router-link to="/">Home</router-link> |
       <router-link to="/about">About</router-link>
     </div>
-    <router-view/>
+    <transition name="fade" mode="out-in">
+      <router-view/>
+    </transition>
   </div>
 </template>
 
 <style lang="scss">
+@import '@/assets/animations/index.scss';
+
 #app {
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
